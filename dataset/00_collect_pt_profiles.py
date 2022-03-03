44bef436c83c6f94d6ab760bf24910828e1a5761
Timothy Gebhard <mail@timothygebhard.de>
2022-03-03 18:02:09 +0100

    Update scripts for data set creation

--- dataset/00_collect_pt_profiles.py
+++ dataset/00_collect_pt_profiles.py
@@ -1,16 +1,14 @@
 """
 This script takes a folder with the full FDL PyATMOS data set as an
-input and collects the PT profiles from it to save them in a single
-HDF file. Specifically, this HDF file will contain one group for each
-PyATMOS simulation (whose name is the hash of that simulation), and
-each group will contain a data set with the altitudes ("ALT"), the
-pressures ("P") and the temperatures ("T").
+input and collects the PT profiles as well as other model parameters,
+such as the gas concentrations and fluxed, from it to save them in a
+single HDF file that is easy to load and work with.
 """
 
 # -----------------------------------------------------------------------------
 # IMPORTS
 # -----------------------------------------------------------------------------
-
+import sys
 from pathlib import Path
 
 import time
@@ -18,6 +16,7 @@ import time
 from tqdm.auto import tqdm
 
 import h5py
+import numpy as np
 import pandas as pd
 
 
@@ -32,10 +31,10 @@ if __name__ == "__main__":
     # -------------------------------------------------------------------------
 
     script_start = time.time()
-    print("\nPREPARE DATASET\n", flush=True)
+    print("\nCOLLECT PT PROFILES FOR PYATMOS DATASET\n", flush=True)
 
     # -------------------------------------------------------------------------
-    # Collect all PT profiles and associated information in an HDF file
+    # Read in summary CSV file
     # -------------------------------------------------------------------------
 
     # Define path to input directory and check if it exists.
@@ -44,8 +43,8 @@ if __name__ == "__main__":
     if not input_dir.exists():
         raise RuntimeError('input directory does not exist!')
 
-    # Read in the pyatmos_summary.csv file, which contains the basic input and
-    # output parameters of all models, as well as the simulation hashes
+    # Read in the pyatmos_summary.csv file, which contains the basic input
+    # and output parameters of all models, as well as the simulation hashes
     print('Reading in pyatmos_summary.csv ...', end=' ', flush=True)
     file_path = input_dir / 'pyatmos_summary.csv'
     summary_df = pd.read_csv(file_path)
@@ -58,36 +57,57 @@ if __name__ == "__main__":
     output_dir.mkdir(exist_ok=True)
     print('Done!', flush=True)
 
-    # Create an output HDF file
-    file_path = output_dir / 'pt_profiles.hdf'
+    # -------------------------------------------------------------------------
+    # Collect pressures, temperatures and altitudes
+    # -------------------------------------------------------------------------
+
+    # Initialize lists for the arrays / vectors that we will collect
+    pressures = []
+    temperatures = []
+    altitudes = []
+    convective = []
+
+    # Loop over all rows in the summary CSV file and collect P, T and ALT
+    print("\nCollecting PT profiles from HDF files:", flush=True)
+    for _, row in tqdm(summary_df.iterrows(), ncols=80, total=n_models):
+
+        # Find the directory that belongs to the current row
+        if row["hash"][0].isdigit():
+            model_dir = input_dir / f'dir_{row["hash"][0]}' / row['hash']
+        else:
+            model_dir = input_dir / f'Dir_alpha' / row['hash']
+
+        # Read in the CSV file that contains the PT profile
+        file_path = model_dir / 'parsed_clima_final.csv'
+        model_df = pd.read_csv(file_path, usecols=['P', 'T', 'ALT', 'CONVEC'])
+
+        # Get the pressure, temperatures, etc. for this model (= vectors)
+        pressures.append(model_df['P'].values)
+        temperatures.append(model_df['T'].values)
+        altitudes.append(model_df['ALT'].values)
+        convective.append(model_df['CONVEC'].values)
+
+    # -------------------------------------------------------------------------
+    # Store everything in an output HDF file
+    # -------------------------------------------------------------------------
+
+    print("\nWriting everything to output HDF file...", end=' ', flush=True)
+
+    # Create an output HDF file and store everything
+    file_path = output_dir / 'pyatmos.hdf'
     with h5py.File(file_path, "w") as hdf_file:
 
-        print("\nCollecting PT profiles in HDF file:", flush=True)
-        for _, row in tqdm(summary_df.iterrows(), ncols=80, total=n_models):
+        # First, store the array-valued columns that we collected
+        hdf_file.create_dataset(name='P', data=np.asarray(pressures))
+        hdf_file.create_dataset(name='T', data=np.asarray(temperatures))
+        hdf_file.create_dataset(name='ALT', data=np.asarray(altitudes))
+        hdf_file.create_dataset(name='CONVEC', data=np.asarray(convective))
 
-            # Find the directory that belongs to the current row
-            if row["hash"][0].isdigit():
-                model_dir = input_dir / f'dir_{row["hash"][0]}' / row['hash']
-            else:
-                model_dir = input_dir / f'Dir_alpha' / row['hash']
+        # Then, store all the columns from the pyatmos_summary.csv file
+        for key in summary_df.keys():
+            hdf_file.create_dataset(name=key, data=summary_df[key].values)
 
-            # Read in the CSV file that contains the PT profile
-            file_path = model_dir / 'parsed_clima_final.csv'
-            model_df = pd.read_csv(file_path)
-
-            # Create a new group in the HDF file for this model
-            group = hdf_file.create_group(name=row['hash'])
-
-            # In this group, store the PT profile (and the altitude)
-            group.create_dataset(name='P', data=model_df['P'].values)
-            group.create_dataset(name='T', data=model_df['T'].values)
-            group.create_dataset(name='ALT', data=model_df['ALT'].values)
-
-            # Additionally, store basic model parameters as attributes
-            for key, value in row.items():
-                if key == 'hash':
-                    continue
-                group.attrs[key] = value
+    print("Done!", flush=True)
 
     # -------------------------------------------------------------------------
     # Postliminaries
--- dataset/01_interpolate_to_pressure_grid.py
+++ dataset/01_interpolate_to_pressure_grid.py
@@ -36,31 +36,27 @@ if __name__ == '__main__':
     # Determine the pressure grid
     # -------------------------------------------------------------------------
 
-    # We need to open the file, loop over all its contents (= all simulations)
-    # and keep track of the respective minimum and maximum pressures, because
-    # ultimately we want a grid that fully overlaps with *all* simulations.
-
     # Define path to the file that holds all the (raw) PT profiles
-    file_path = get_dataset_dir() / 'output' / 'pt_profiles.hdf'
+    file_path = get_dataset_dir() / 'output' / 'pyatmos.hdf'
     if not file_path.exists():
         raise FileNotFoundError(f'{file_path} does not exist!')
 
-    # Keep track of the pressure values
-    p_min = -np.infty
-    p_max = np.infty
-
-    # Open the file, loop over all simulations, and update min / max pressure
-    print('Finding limits for pressure grid:')
+    # Open file and compute minimum and maximum of each pressure grid
+    print('Determining pressure grid...', end=' ', flush=True)
     with h5py.File(file_path, 'r') as hdf_file:
-        for key in track(hdf_file.keys(), description=''):
-            pressures = hdf_file[key]['P']
-            p_min = max(p_min, min(pressures))
-            p_max = min(p_max, max(pressures))
+        pressures = np.array(hdf_file['P'])
+        minima = np.min(pressures, axis=1)
+        maxima = np.max(pressures, axis=1)
 
-    print('')
-    print('p_min:', p_min)
-    print('p_max:', p_max)
-    print('')
+    # Determine interval that is covered by *all* profiles
+    p_min = max(minima)
+    p_max = min(maxima)
+
+    print('Done!', flush=True)
+    print('', flush=True)
+    print('p_min:', p_min, flush=True)
+    print('p_max:', p_max, flush=True)
+    print('', flush=True)
 
     # Define a pressure grid for interpolation
     # Note: It is *not* obvious how to do this. The PyATMOS data are based on
@@ -71,18 +67,44 @@ if __name__ == '__main__':
     # we make to get *something* to work with...
     pressure_grid = np.geomspace(p_min, p_max, 100)
 
-    # Store the (interpolated) temperatures and hashes
-    temperatures = []
-    hashes = []
+    # -------------------------------------------------------------------------
+    # Copy the original pyatmos.hdf file, but remove P and T
+    # -------------------------------------------------------------------------
 
-    # Read in the HDF file and interpolate the altitude-temperature profiles
-    print('Interpolating PT profiles:', flush=True)
+    # Define source (src) and destination (dst) files for copying datasets
+    src_file_path = get_dataset_dir() / 'output' / 'pyatmos.hdf'
+    dst_file_path = get_dataset_dir() / 'output' / 't_for_fixed_p.hdf'
+
+    with h5py.File(src_file_path, 'r') as src_file:
+        with h5py.File(dst_file_path, 'w') as dst_file:
+
+            # Loop over keys in source file to copy them
+            for key in src_file.keys():
+
+                # Skip altitudes, pressures and temperatures
+                if key in ('ALT', 'P', 'T'):
+                    continue
+
+                # Copy the other datasets from source to destination
+                print(f'Copying {key}...', end=' ', flush=True)
+                src_file.copy(source=key, dest=dst_file)
+                print('Done!', flush=True)
+
+    # -------------------------------------------------------------------------
+    # Interpolate temperatures to pressure_grid
+    # -------------------------------------------------------------------------
+
+    # Keep track of interpolated temperatures
+    temperatures = []
+
+    # Open the source file, loop over PT arrays and interpolate to grid
+    print('\nInterpolating PT profiles:', flush=True)
     with h5py.File(file_path.as_posix(), 'r') as hdf_file:
-        for key in track(hdf_file.keys(), description=''):
+        for idx in track(range(len(hdf_file['P'])), description=''):
 
             # Get the values for the pressure and temperature
-            pressure = np.array(hdf_file[key]['P'])
-            temperature = np.array(hdf_file[key]['T'])
+            pressure = np.array(hdf_file['P'][idx])
+            temperature = np.array(hdf_file['T'][idx])
 
             # Set up an interpolator (use linear interpolation for now)
             interpolator = interp1d(x=pressure, y=temperature, kind='linear')
@@ -91,13 +113,14 @@ if __name__ == '__main__':
             temperature = interpolator(pressure_grid)
             temperatures.append(temperature)
 
-            # Store the hash of the simulation
-            hashes.append(key)
+    # -------------------------------------------------------------------------
+    # Save pressure_grid and interpolated temperatures to output HDF file
+    # -------------------------------------------------------------------------
 
     # Store the interpolated temperatures as an HDF file
-    print('Saving results to HDF file...', end=' ', flush=True)
-    file_path = get_dataset_dir() / 'output' / 't_for_fixed_p.hdf'
-    with h5py.File(file_path.as_posix(), 'w') as hdf_file:
+    print('\nSaving interpolated temperatures...', end=' ', flush=True)
+    with h5py.File(dst_file_path, 'a') as hdf_file:
+
         hdf_file.create_dataset(
             name='pressure_grid',
             data=pressure_grid,
@@ -108,12 +131,7 @@ if __name__ == '__main__':
             data=np.array(temperatures),
             dtype=np.float32,
         )
-        hdf_file.create_dataset(
-            name='hashes',
-            data=np.array(hashes, dtype='S25'),
-            dtype='S25',
-            
-        )
+
     print('Done!', flush=True)
 
     # -------------------------------------------------------------------------
