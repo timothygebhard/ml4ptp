80e873b8eb0866f623ccd22fef82faad6c6e8a42
Timothy Gebhard <mail@timothygebhard.de>
2022-03-09 20:33:05 +0100

    Change *.tar to *.zip

--- datasets/ms-100k/input/download.sh
+++ datasets/ms-100k/input/download.sh
@@ -2,11 +2,11 @@
 
 printf "\n"
 printf "Downloading dataset... "
-wget -q https://polybox.ethz.ch/index.php/s/NJDNvggyYIRWi9L/download -O dataset.tar;
+wget -q https://polybox.ethz.ch/index.php/s/NJDNvggyYIRWi9L/download -O dataset.zip;
 printf "Done! \n"
 
 printf "Unpacking dataset... "
-tar -xf dataset.tar ;
+unzip -q dataset.zip ;
 printf "Done! \n"
 
 printf "Moving files... "
@@ -16,9 +16,8 @@ mv PT_Seager_Database/seager_timmy_parameters.txt parameters.txt ;
 mv PT_Seager_Database/seager_timmy_profiles.txt temperatures.txt ;
 printf "Done! \n"
 
-
-printf "Deleting *.tar file... "
-rm dataset.tar ;
+printf "Deleting *.zip file... "
+rm dataset.zip ;
 rm -r PT_Seager_Database ;
 printf "Done! \n"
 printf "\n"
