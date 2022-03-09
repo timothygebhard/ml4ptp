6b5bf9583d2a2aabaafa85c558977af6b7cead8b
Timothy Gebhard <mail@timothygebhard.de>
2022-03-09 17:49:14 +0100

    Add / update functions to get paths of data sets

--- ml4ptp/config.py
+++ ml4ptp/config.py
@@ -16,11 +16,40 @@ import ml4ptp
 # -----------------------------------------------------------------------------
 
 def get_ml4ptp_dir() -> Path:
-    return Path(ml4ptp.__file__).parent.parent
+    return Path(ml4ptp.__file__).parent.parent.resolve()
 
 
-def get_dataset_dir() -> Path:
-    return get_ml4ptp_dir() / 'dataset'
+def get_datasets_dir() -> Path:
+    return get_ml4ptp_dir() / 'datasets'
+
+
+def get_dataset_path(dataset: str) -> Path:
+    """
+    Get the path to the HDF file that contains the given `dataset`.
+
+    Args:
+        dataset: Name of the dataset (e.g., "pyatmos").
+
+    Returns:
+        The path to the HDF file for the given `dataset`.
+    """
+
+    # Define path to output directory
+    path = get_datasets_dir() / dataset / 'output'
+
+    # Depending on the dataset, append the name of the HDF file
+    if dataset == 'pyatmos':
+        path /= 't_for_fixed_p.hdf'
+    elif dataset == 'ms-100k':
+        path /= 'ms-100k.hdf'
+    else:
+        raise ValueError(f'Illegal value for dataset: {dataset}')
+
+    # Double-check that the target file exists
+    if not path.exists():
+        raise RuntimeError(f'Target file ({path}) does not exist!')
+
+    return path
 
 
 def get_experiments_dir() -> Path:
