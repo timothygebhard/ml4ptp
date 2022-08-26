97a7bd379262cdde0cb2ed46699e356af70ca130
Timothy Gebhard <mail@timothygebhard.de>
2022-08-26 03:40:02 +0200

    Add new encoder architectures and unit tests

--- ml4ptp/encoders.py
+++ ml4ptp/encoders.py
@@ -20,7 +20,7 @@ from ml4ptp.mixins import NormalizerMixin
 # DEFINITIONS
 # -----------------------------------------------------------------------------
 
-class Encoder(nn.Module, NormalizerMixin):
+class ConvolutionalEncoder(nn.Module, NormalizerMixin):
 
     def __init__(
         self,
@@ -68,5 +68,100 @@ class Encoder(nn.Module, NormalizerMixin):
 
         # Compute forward pass through encoder to get latent variable z
         z = self.layers(encoder_input)
+
+        return z
+
+
+class MLPEncoder(nn.Module, NormalizerMixin):
+
+    def __init__(
+        self,
+        input_size: int,
+        latent_size: int,
+        layer_size: int,
+        T_mean: float,
+        T_std: float,
+    ) -> None:
+
+        super().__init__()
+
+        # Store constructor arguments
+        self.input_size = input_size
+        self.latent_size = latent_size
+        self.layer_size = layer_size
+        self.T_mean = T_mean
+        self.T_std = T_std
+
+        # Define encoder architecture
+        self.layers: Callable[[torch.Tensor], torch.Tensor] = nn.Sequential(
+            nn.Linear(2 * self.input_size, self.layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.layer_size, self.layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.layer_size, self.layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.layer_size, self.latent_size),
+        )
+
+    def forward(self, log_P: torch.Tensor, T: torch.Tensor) -> torch.Tensor:
+
+        # Normalize temperatures and construct encoder input
+        normalized_T = self.normalize(T)
+        encoder_input = torch.column_stack((log_P, normalized_T))
+
+        # Compute forward pass through encoder to get latent variable z
+        z = self.layers(encoder_input)
+
+        return z
+
+
+class CNPEncoder(nn.Module, NormalizerMixin):
+
+    def __init__(
+        self,
+        latent_size: int,
+        layer_size: int,
+        T_mean: float,
+        T_std: float,
+    ) -> None:
+
+        super().__init__()
+
+        # Store constructor arguments
+        self.latent_size = latent_size
+        self.layer_size = layer_size
+        self.T_mean = T_mean
+        self.T_std = T_std
+
+        # Define encoder architecture
+        self.layers: Callable[[torch.Tensor], torch.Tensor] = nn.Sequential(
+            nn.Linear(2, self.layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.layer_size, self.layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.layer_size, self.layer_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.layer_size, self.latent_size),
+        )
+
+    def forward(self, log_P: torch.Tensor, T: torch.Tensor) -> torch.Tensor:
+
+        # Normalize temperatures
+        normalized_T = self.normalize(T)
+
+        # Construct encoder input: Reshape grid into batch dimension
+        batch_size, grid_size = log_P.shape
+        log_P = log_P.reshape(batch_size * grid_size, 1)
+        normalized_T = normalized_T.reshape(batch_size * grid_size, 1)
+        encoder_input = torch.column_stack((log_P, normalized_T))
+
+        # Compute forward pass through encoder to get latent variable z
+        z_values = self.layers(encoder_input)
+
+        # Reshape to get grid dimension back
+        z_values = z_values.reshape(batch_size, grid_size, self.latent_size)
+
+        # Aggregate along grid dimension to get final z
+        z = torch.mean(z_values, dim=1)
 
         return z
--- tests/unit/test_encoders.py
+++ tests/unit/test_encoders.py
@@ -12,7 +12,7 @@ import numpy as np
 import pytest
 import torch
 
-from ml4ptp.encoders import Encoder
+from ml4ptp.encoders import ConvolutionalEncoder, MLPEncoder, CNPEncoder
 
 
 # -----------------------------------------------------------------------------
@@ -28,13 +28,43 @@ def data() -> Tuple[torch.Tensor, torch.Tensor]:
     return log_P, T
 
 
-def test__encoder(data: Tuple[torch.Tensor, torch.Tensor]) -> None:
+def test__convolutional_encoder(
+    data: Tuple[torch.Tensor, torch.Tensor]
+) -> None:
 
     torch.manual_seed(42)
-    encoder = Encoder(latent_size=5, layer_size=16, T_mean=1, T_std=2)
+    encoder = ConvolutionalEncoder(
+        latent_size=5, layer_size=16, T_mean=1, T_std=2
+    )
 
     # Case 1
     log_P, T = data
     output = encoder.forward(log_P=log_P, T=T)
     assert output.shape == (17, 5)
     assert np.isclose(output.mean().item(), 0.013009665533900261)
+
+
+def test__mlp_encoder(data: Tuple[torch.Tensor, torch.Tensor]) -> None:
+
+    torch.manual_seed(42)
+    encoder = MLPEncoder(
+        input_size=29, latent_size=5, layer_size=16, T_mean=1, T_std=2
+    )
+
+    # Case 1
+    log_P, T = data
+    output = encoder.forward(log_P=log_P, T=T)
+    assert output.shape == (17, 5)
+    assert np.isclose(output.mean().item(), 0.0008047096198424697)
+
+
+def test__cnp_encoder(data: Tuple[torch.Tensor, torch.Tensor]) -> None:
+
+    torch.manual_seed(42)
+    encoder = CNPEncoder(latent_size=5, layer_size=16, T_mean=1, T_std=2)
+
+    # Case 1
+    log_P, T = data
+    output = encoder.forward(log_P=log_P, T=T)
+    assert output.shape == (17, 5)
+    assert np.isclose(output.mean().item(), 0.00554615119472146)
