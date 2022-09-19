20c704edf3679f9808ec85054c611825fb357bff
Timothy Gebhard <mail@timothygebhard.de>
2022-09-19 21:51:07 +0200

    Drop ConvolutionalEncoder

--- ml4ptp/encoders.py
+++ ml4ptp/encoders.py
@@ -12,65 +12,13 @@ from typing import Callable
 import torch
 import torch.nn as nn
 
-from ml4ptp.layers import get_mlp_layers, Mean
+from ml4ptp.layers import get_mlp_layers
 from ml4ptp.mixins import NormalizerMixin
 
 
 # -----------------------------------------------------------------------------
 # DEFINITIONS
 # -----------------------------------------------------------------------------
-
-class ConvolutionalEncoder(nn.Module, NormalizerMixin):
-
-    def __init__(
-        self,
-        latent_size: int,
-        layer_size: int,
-        T_mean: float,
-        T_std: float,
-    ) -> None:
-
-        super().__init__()
-
-        # Store constructor arguments
-        self.latent_size = latent_size
-        self.layer_size = layer_size
-        self.T_mean = T_mean
-        self.T_std = T_std
-
-        # Define encoder architecture
-        self.layers: Callable[[torch.Tensor], torch.Tensor] = nn.Sequential(
-            nn.Conv1d(
-                in_channels=2,
-                out_channels=self.layer_size,
-                kernel_size=(4,),
-            ),
-            nn.LeakyReLU(),
-            nn.Conv1d(
-                in_channels=self.layer_size,
-                out_channels=self.layer_size,
-                kernel_size=(4,),
-            ),
-            nn.LeakyReLU(),
-            nn.Conv1d(
-                in_channels=self.layer_size,
-                out_channels=self.latent_size,
-                kernel_size=(4,),
-            ),
-            Mean(dim=2),
-        )
-
-    def forward(self, log_P: torch.Tensor, T: torch.Tensor) -> torch.Tensor:
-
-        # Normalize temperatures and construct encoder input
-        normalized_T = self.normalize(T)
-        encoder_input = torch.stack((log_P, normalized_T), dim=1)
-
-        # Compute forward pass through encoder to get latent variable z
-        z = self.layers(encoder_input)
-
-        return z
-
 
 class MLPEncoder(nn.Module, NormalizerMixin):
 
