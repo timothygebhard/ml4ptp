6e9701e10bb86e94f1c7ed6762e2d2ee23d3d882
Timothy Gebhard <mail@timothygebhard.de>
2022-09-23 16:58:46 +0200

    Refactor evaluation methods
    - Use two different methods to get initial and refined evaluation
    - Use Adam instead of L-BFGS to refine the initial value of z

--- ml4ptp/evaluation.py
+++ ml4ptp/evaluation.py
@@ -8,86 +8,139 @@ Utility functions for evaluation on the test set.
 
 from typing import Tuple
 
-from rich.progress import track
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS
+from tqdm.auto import tqdm
 
 import numpy as np
 import torch
 
 from ml4ptp.models import Model
-from ml4ptp.optimization import optimize_z_with_lbfgs
+from ml4ptp.utils import get_batch_idx
 
 
 # -----------------------------------------------------------------------------
 # DEFINTIONS
 # -----------------------------------------------------------------------------
 
-def evaluate_on_test_set(
+def get_initial_predictions(
     model: Model,
     test_dataloader: EVAL_DATALOADERS,
-    device: torch.device = torch.device('cpu'),
-    n_epochs: int = 100,
-    batch_size: int = 128,
-    history_size: int = 10,
-) -> Tuple[np.ndarray, ...]:
+    device: torch.device,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
 
     # Make sure model is in evaluation mode
     model = model.eval()
 
     # Keep track of batch-wise results
     z_initial_all = []
-    z_optimal_all = []
     T_true_all = []
     log_P_all = []
     T_pred_initial_all = []
-    T_pred_optimal_all = []
 
-    # Loop over data in batches
-    for log_P_, T in track(
-        sequence=test_dataloader,
-        description='Optimizing z:',
-    ):
+    # Loop over the test set in batches to get z and T_pred
+    for _log_P, _T_true in tqdm(test_dataloader, ncols=80):
 
-        # Compute "normal" pass through the model (encoder and decoder)
-        z_initial_, T_pred_initial_ = model(
-            log_P=log_P_.to(device), T=T.to(device)
-        )
-
-        # Use the value of z from the encoder as a starting point for an
-        # optimizer to find the best input value to the decoder
-        z_optimal_, T_pred_optimal_ = optimize_z_with_lbfgs(
-            z_initial=z_initial_.detach(),
-            log_P=log_P_,
-            T_true=T,
-            decoder=model.decoder,
-            n_epochs=n_epochs,
-            batch_size=batch_size,
-            device=device,
-            history_size=history_size,
+        # Pass through encoder / decoder to get initial z and T_pred
+        _z_initial, _T_pred_initial = model(
+            log_P=_log_P.to(device), T=_T_true.to(device)
         )
 
         # Store batch results (as numpy array on CPU!)
-        z_initial_all.append(z_initial_.detach().cpu().numpy())
-        z_optimal_all.append(z_optimal_.detach().cpu().numpy())
-        T_true_all.append(T.detach().cpu().numpy())
-        log_P_all.append(log_P_.detach().cpu().numpy())
-        T_pred_initial_all.append(T_pred_initial_.detach().cpu().numpy())
-        T_pred_optimal_all.append(T_pred_optimal_.detach().cpu().numpy())
+        z_initial_all.append(_z_initial.detach().cpu().numpy())
+        T_true_all.append(_T_true.detach().cpu().numpy())
+        log_P_all.append(_log_P.detach().cpu().numpy())
+        T_pred_initial_all.append(_T_pred_initial.detach().cpu().numpy())
 
         # Add some sanity checks to ensure we are not just producing NaNs
         assert not np.any(np.isnan(z_initial_all[-1]))
-        assert not np.any(np.isnan(z_optimal_all[-1]))
         assert not np.any(np.isnan(T_true_all[-1]))
         assert not np.any(np.isnan(log_P_all[-1]))
         assert not np.any(np.isnan(T_pred_initial_all[-1]))
-        assert not np.any(np.isnan(T_pred_optimal_all[-1]))
 
     # Merge batch results
     z_initial = np.row_stack(z_initial_all)
-    z_optimal = np.row_stack(z_optimal_all)
     T_true = np.row_stack(T_true_all)
     log_P = np.row_stack(log_P_all)
     T_pred_initial = np.row_stack(T_pred_initial_all)
-    T_pred_optimal = np.row_stack(T_pred_optimal_all)
 
-    return z_initial, z_optimal, T_true, log_P, T_pred_initial, T_pred_optimal
+    return z_initial, T_true, log_P, T_pred_initial
+
+
+def get_refined_predictions(
+    model: Model,
+    z_initial: np.ndarray,
+    T_true: np.ndarray,
+    log_P: np.ndarray,
+    device: torch.device,
+    n_epochs: int = 50,
+    batch_size: int = 128,
+) -> Tuple[np.ndarray, np.ndarray]:
+
+    # Make sure model is in evaluation mode
+    model = model.eval()
+    decoder = model.decoder
+
+    # Get batch indices for looping over the inputs
+    batch_idx = get_batch_idx(a=z_initial, batch_size=batch_size)
+
+    # Store the refined results that we obtain through optimization
+    z_refined_all = []
+    T_pred_refined_all = []
+
+    # Loop over data in batches
+    for idx in tqdm(batch_idx, ncols=80):
+
+        # Select batch and create tensors on the target device
+        _z_refined = (
+            torch.from_numpy(z_initial[idx]).to(device).requires_grad_(True)
+        )
+        _T_true = (
+            torch.from_numpy(T_true[idx]).to(device).requires_grad_(False)
+        )
+        _log_P = torch.from_numpy(log_P[idx]).to(device).requires_grad_(False)
+
+        # Create a new optimizer that optimizes _z_refined plus LR scheduler
+        optimizer = torch.optim.AdamW(params=[_z_refined], lr=3e-2)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, step_size=10, gamma=0.5
+        )
+
+        # Compute errors before refinement
+        _T_pred = decoder(z=_z_refined, log_P=_log_P)
+        errors_initial = torch.nn.functional.mse_loss(_T_pred, _T_true)
+
+        # Run for the desired number of optimization steps
+        for epoch in range(n_epochs):
+
+            # Reset the gradients
+            optimizer.zero_grad()
+
+            # Compute T_pred, compute loss, and take step with optimizer
+            _T_pred = decoder(z=_z_refined, log_P=_log_P)
+            loss = torch.nn.functional.mse_loss(_T_pred, _T_true)
+            loss.backward()  # type: ignore
+            optimizer.step()
+            scheduler.step()
+
+        # Compute T_pred for final value of z, plus errors after refinement
+        _T_pred = decoder(z=_z_refined, log_P=_log_P)
+        errors_refined = torch.nn.functional.mse_loss(_T_pred, _T_true)
+
+        # Find profiles where the refinement did not decrease the error
+        not_improved = errors_refined.squeeze() > errors_initial.squeeze()
+        _z_refined = _z_refined.detach().cpu().numpy()
+        _z_refined[not_improved] = z_initial[idx][not_improved]
+
+        # Store batch results (as numpy array on CPU!)
+        z_refined_all.append(_z_refined)
+        T_pred_refined_all.append(_T_pred.detach().cpu().numpy())
+
+        # Add some sanity checks to ensure we are not just producing NaNs
+        assert not np.any(np.isnan(z_refined_all[-1]))
+        assert not np.any(np.isnan(T_pred_refined_all[-1]))
+
+    # Merge batch results
+    z_refined = np.row_stack(z_refined_all)
+    T_pred_refined = np.row_stack(T_pred_refined_all)
+
+    return z_refined, T_pred_refined
