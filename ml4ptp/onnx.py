60490bb8369d517822ce98f0f64b91ee2b695218
Timothy Gebhard <mail@timothygebhard.de>
2022-11-28 23:49:32 +0100

    Add onnx.py

---
+++ ml4ptp/onnx.py
@@ -0,0 +1,59 @@
+"""
+Methods for dealing with ONNX models.
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from typing import Union
+
+import numpy as np
+import onnxruntime
+
+
+# -----------------------------------------------------------------------------
+# DEFINITIONS
+# -----------------------------------------------------------------------------
+
+class ONNXEncoder:
+
+    def __init__(self, path_or_bytes: Union[str, bytes]) -> None:
+
+        # Create a session for the ONNX model
+        self.session = onnxruntime.InferenceSession(path_or_bytes)
+
+    def __call__(self, log_P: np.ndarray, T: np.ndarray) -> np.ndarray:
+
+        # Run the model
+        inputs = {
+            'log_P': log_P.astype(np.float32),
+            'T': T.astype(np.float32),
+        }
+        outputs = self.session.run(None, inputs)
+
+        return np.asarray(outputs[0])
+
+
+class ONNXDecoder:
+    """
+    A thin wrapper around ``onnxruntime.InferenceSession`` that can
+    load an ONNX model from a file path or a byte string and provides
+    a simple ``__call__`` method that can be used to run the model.
+    """
+
+    def __init__(self, path_or_bytes: Union[str, bytes]) -> None:
+
+        # Create a session for the ONNX model
+        self.session = onnxruntime.InferenceSession(path_or_bytes)
+
+    def __call__(self, z: np.ndarray, log_P: np.ndarray) -> np.ndarray:
+
+        # Run the model
+        inputs = {
+            'z': z.astype(np.float32),
+            'log_P': log_P.astype(np.float32),
+        }
+        outputs = self.session.run(None, inputs)
+
+        return np.asarray(outputs[0])
