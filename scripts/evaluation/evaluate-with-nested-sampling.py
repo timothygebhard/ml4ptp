5fe9b254a4346dc6bba75281bbce27ad47571551
Timothy Gebhard <mail@timothygebhard.de>
2022-11-28 22:36:18 +0100

    Update evaluation script to work with ONNX models

--- scripts/evaluation/evaluate-with-nested-sampling.py
+++ scripts/evaluation/evaluate-with-nested-sampling.py
@@ -6,25 +6,29 @@ Evaluate model on test set.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
+from functools import partial
 from pathlib import Path
+from typing import Dict, Union
 
 import argparse
-import io
 import logging
 import time
 
-from joblib import delayed, Parallel
-from tqdm.auto import tqdm
+# from joblib import delayed, Parallel
+from p_tqdm import p_map
+
+# from tqdm.auto import tqdm
 
 import h5py
 import scipy.stats
-import torch
 import numpy as np
+import onnx
 import pandas as pd
 import ultranest
 
 from ml4ptp.config import load_config
 from ml4ptp.data_modules import DataModule
+from ml4ptp.onnx import ONNXEncoder, ONNXDecoder
 from ml4ptp.paths import expandvars
 from ml4ptp.utils import get_number_of_available_cores
 
@@ -39,17 +43,17 @@ def get_cli_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--experiment-dir',
-        default=(
-            '/Users/timothy/Desktop/projects/ml4ptp/experiments/playground'
-        ),
-        # required=True,
+        default='$ML4PTP_EXPERIMENTS_DIR/pyatmos/default/latent-size-2',
+        required=True,
         help='Path to the experiment directory with the config.yaml',
     )
     parser.add_argument(
         '--run-dir',
-        default=0,
+        default=(
+            '$ML4PTP_EXPERIMENTS_DIR/pyatmos/default/latent-size-2/runs/run_0'
+        ),
         type=str,
-        # required=True,
+        required=True,
         help=(
             'Path to the directory of the run to evaluate. Should contain the '
             'saved encoder and decoder models: encoder.pt and decoder.pt.'
@@ -67,39 +71,39 @@ def get_cli_args() -> argparse.Namespace:
 
 
 def find_optimal_z_with_nested_sampling(
-    log_P: torch.Tensor,
-    T_true: torch.Tensor,
-    encoder_buffer: io.BytesIO,
-    decoder_buffer: io.BytesIO,
+    log_P: np.ndarray,
+    T_true: np.ndarray,
+    encoder_bytes: bytes,
+    decoder_bytes: bytes,
     latent_size: int,
 ) -> dict:
 
-    # Load decoder from buffer
-    loc = torch.device('cpu')
-    encoder = torch.jit.load(encoder_buffer, map_location=loc)  # type: ignore
-    decoder = torch.jit.load(decoder_buffer, map_location=loc)  # type: ignore
+    # Load encoder and decoder from byte strings
+    encoder = ONNXEncoder(encoder_bytes)
+    decoder = ONNXDecoder(decoder_bytes)
 
     # Prepare dict with results
-    results = dict()
+    results: Dict[str, Union[float, np.ndarray]] = dict()
 
     # -------------------------------------------------------------------------
     # Get initial guess for z from encoder and compute error
     # -------------------------------------------------------------------------
 
-    with torch.no_grad():
+    z_initial = encoder(log_P=log_P, T=T_true)
+    T_pred_initial = decoder(log_P=log_P, z=z_initial)
+    mse_initial = float(np.mean((T_pred_initial - T_true) ** 2))
 
-        z_initial = encoder(log_P=log_P, T=T_true)
-        T_pred_initial = decoder(log_P=log_P, z=z_initial)
-        mse_initial = float(torch.mean((T_pred_initial - T_true) ** 2))
-
-        results['z_initial'] = z_initial.numpy()
-        results['T_pred_initial'] = T_pred_initial.numpy()
-        results['mse_initial'] = mse_initial
-        results['T_true'] = T_true.numpy()
+    results['z_initial'] = z_initial
+    results['T_pred_initial'] = T_pred_initial
+    results['mse_initial'] = mse_initial
+    results['T_true'] = T_true
 
     # -------------------------------------------------------------------------
     # Define (Gaussian) prior and likelihood
     # -------------------------------------------------------------------------
+
+    # noinspection PyUnresolvedReferences
+    gaussdistribution = scipy.stats.norm(0, 1)
 
     def gaussian_prior(cube: np.ndarray) -> np.ndarray:
         """
@@ -107,9 +111,6 @@ def find_optimal_z_with_nested_sampling(
         """
 
         params = cube.copy()
-        # noinspection PyUnresolvedReferences
-        gaussdistribution = scipy.stats.norm(0, 1)
-
         for i in range(latent_size):
             params[:, i] = gaussdistribution.ppf(cube[:, i])
 
@@ -120,23 +121,12 @@ def find_optimal_z_with_nested_sampling(
         Likelihood for comparing PT profiles (= negative MSE).
         """
 
-        z = torch.from_numpy(params).float()
-        log_P_ = torch.from_numpy(np.tile(log_P, (z.shape[0], 1))).float()
-
-        with torch.no_grad():
-            T_pred = (
-                decoder(
-                    log_P=log_P_,
-                    z=z,
-                )
-                .detach()
-                .numpy()
-                .squeeze()
-            )
-
+        z = params.copy()
+        log_P_ = np.tile(log_P, (z.shape[0], 1))
         T_true_ = np.tile(T_true, (z.shape[0], 1))
 
-        mse = np.asarray(np.mean(np.square(T_true_ - T_pred), axis=1))
+        T_pred = decoder(log_P=log_P_, z=z)
+        mse = np.asarray(np.mean((T_true_ - T_pred) ** 2, axis=1))
 
         return -mse
 
@@ -166,15 +156,14 @@ def find_optimal_z_with_nested_sampling(
     # Decode refined z and compute error
     # -------------------------------------------------------------------------
 
-    z_refined = torch.FloatTensor(result['maximum_likelihood']['point'])
-    results['z_refined'] = z_refined.numpy()
+    z_refined = np.asarray(result['maximum_likelihood']['point'])
+    results['z_refined'] = z_refined.squeeze()
     results['niter'] = int(result['niter'])
 
-    with torch.no_grad():
-        T_pred_refined = decoder(log_P=log_P, z=z_refined)
-        mse_refined = float(torch.mean((T_pred_refined - T_true) ** 2))
-        results['T_pred_refined'] = T_pred_refined.numpy()
-        results['mse_refined'] = mse_refined
+    T_pred_refined = decoder(log_P=log_P, z=np.atleast_2d(z_refined))
+    mse_refined = float(np.mean((T_pred_refined - T_true) ** 2))
+    results['T_pred_refined'] = T_pred_refined
+    results['mse_refined'] = mse_refined
 
     return results
 
@@ -224,18 +213,16 @@ if __name__ == "__main__":
     datamodule.prepare_data()
     print('Done!', flush=True)
 
-    # Load the trained encoder (as a buffer that can be pickled)
+    # Load the trained encoder (as a byte string that can be serialized)
     print('Loading trained encoder...', end=' ', flush=True)
-    file_path = run_dir / 'encoder.pt'
-    with open(file_path, 'rb') as f:
-        encoder_buffer = io.BytesIO(f.read())
+    file_path = run_dir / 'encoder.onnx'
+    encoder_bytes = onnx.load(file_path.as_posix()).SerializeToString()
     print('Done!', flush=True)
 
-    # Load the trained decoder
+    # Load the trained decoder (as a byte string that can be serialized)
     print('Loading trained decoder...', end=' ', flush=True)
-    file_path = run_dir / 'decoder.pt'
-    with open(file_path, 'rb') as f:
-        decoder_buffer = io.BytesIO(f.read())
+    file_path = run_dir / 'decoder.onnx'
+    decoder_bytes = onnx.load(file_path.as_posix()).SerializeToString()
     print('Done!\n', flush=True)
 
     # -------------------------------------------------------------------------
@@ -246,15 +233,47 @@ if __name__ == "__main__":
     print(f'Number of available cores: {n_jobs}\n', flush=True)
 
     print('Fitting:', flush=True)
-    results = Parallel(n_jobs=n_jobs)(
-        delayed(find_optimal_z_with_nested_sampling)(
-            log_P=log_P,
-            T_true=T_true,
-            encoder_buffer=encoder_buffer,
-            decoder_buffer=decoder_buffer,
+
+    # # With joblib parallelization
+    # results = Parallel(n_jobs=n_jobs)(
+    #     delayed(find_optimal_z_with_nested_sampling)(
+    #         log_P=log_P.numpy(),
+    #         T_true=T_true.numpy(),
+    #         encoder_bytes=encoder_bytes,
+    #         decoder_bytes=decoder_bytes,
+    #         latent_size=latent_size,
+    #     )
+    #     for log_P, T_true in tqdm(list(datamodule.test_dataloader())[:16])
+    # )
+
+    # # Without parallelization
+    # results = []
+    # for log_P, T_true in tqdm(list(datamodule.test_dataloader())[:3]):
+    #     result = find_optimal_z_with_nested_sampling(
+    #         log_P=log_P.numpy(),
+    #         T_true=T_true.numpy(),
+    #         encoder_bytes=encoder_bytes,
+    #         decoder_bytes=decoder_bytes,
+    #         latent_size=latent_size,
+    #     )
+    #     results.append(result)
+
+    # With p_map parallelization
+    log_P, T_true = [], []
+    for log_P_, T_true_ in list(datamodule.test_dataloader())[:16]:
+        log_P.append(log_P_.numpy())
+        T_true.append(T_true_.numpy())
+    
+    results = p_map(
+        partial(
+            find_optimal_z_with_nested_sampling,
+            encoder_bytes=encoder_bytes,
+            decoder_bytes=decoder_bytes,
             latent_size=latent_size,
-        )
-        for log_P, T_true in tqdm(list(datamodule.test_dataloader()))
+        ),
+        log_P,
+        T_true,
+        num_cpus=n_jobs,
     )
 
     # Convert results (i.e., list of dicts) to a dataframe for easier handling
