bec4ffd5d177d8ee85d372807105234d6533b5d8
Timothy Gebhard <mail@timothygebhard.de>
2022-12-13 23:19:49 +0100

    Add `ConvolutionalEncoder`

--- ml4ptp/encoders.py
+++ ml4ptp/encoders.py
@@ -12,7 +12,7 @@ from typing import Any, Dict
 import torch
 import torch.nn as nn
 
-from ml4ptp.layers import get_mlp_layers
+from ml4ptp.layers import get_mlp_layers, Squeeze
 from ml4ptp.mixins import NormalizerMixin
 
 
@@ -21,7 +21,6 @@ from ml4ptp.mixins import NormalizerMixin
 # -----------------------------------------------------------------------------
 
 class MLPEncoder(nn.Module, NormalizerMixin):
-
     def __init__(
         self,
         input_size: int,
@@ -177,6 +176,98 @@ class ModifiedMLPEncoder(nn.Module, NormalizerMixin):
 
         # Compute forward pass through second encoder part
         z: torch.Tensor = self.layers_2(output)
+
+        return z
+
+
+class ConvolutionalEncoder(nn.Module, NormalizerMixin):
+
+    def __init__(
+        self,
+        input_size: int,
+        latent_size: int,
+        layer_size: int,
+        n_layers: int,
+        normalization: Dict[str, Any],
+        n_channels: int = 64,
+        batch_norm: bool = False,
+    ) -> None:
+
+        super().__init__()
+
+        # Store constructor arguments
+        self.input_size = input_size
+        self.latent_size = latent_size
+        self.layer_size = layer_size
+        self.n_layers = n_layers
+        self.normalization = normalization
+        self.batch_norm = batch_norm
+
+        # Define encoder architecture
+        self.convnet = nn.Sequential(
+            torch.nn.Conv1d(
+                in_channels=2, out_channels=n_channels, kernel_size=1
+            ),
+            torch.nn.LeakyReLU(),
+            torch.nn.Conv1d(
+                in_channels=n_channels, out_channels=n_channels, kernel_size=1
+            ),
+            torch.nn.LeakyReLU(),
+            torch.nn.Conv1d(
+                in_channels=n_channels, out_channels=n_channels, kernel_size=1
+            ),
+            torch.nn.LeakyReLU(),
+            torch.nn.Conv1d(
+                in_channels=n_channels, out_channels=1, kernel_size=1
+            ),
+            torch.nn.LeakyReLU(),
+            Squeeze(),
+        )
+        self.mlp: torch.nn.Sequential = get_mlp_layers(
+            input_size=self.input_size,
+            layer_size=self.layer_size,
+            n_layers=self.n_layers,
+            output_size=self.latent_size,
+            activation='leaky_relu',
+            final_tanh=True,
+            batch_norm=batch_norm,
+        )
+
+        # Initialize weights
+        self.initialize_weights()
+
+    def initialize_weights(self) -> None:
+        """
+        Initialize weights of encoder layers. Preliminary experiments
+        show that this can help to prevent the encoder from predicting
+        all zeros (which leads to the model not learning anything).
+        """
+
+        for layer in self.convnet:
+            if isinstance(layer, nn.Conv1d):
+                nn.init.kaiming_normal_(layer.weight)
+                nn.init.constant_(layer.bias, 0.01)  # type: ignore
+
+        for layer in self.mlp:
+            if isinstance(layer, nn.Linear):
+                nn.init.kaiming_uniform_(layer.weight)
+                nn.init.constant_(layer.bias, 0.01)
+
+    def forward(self, log_P: torch.Tensor, T: torch.Tensor) -> torch.Tensor:
+
+        # Stack log_P and T together into a tensor with 2 channels, that is,
+        # the shape is `(batch_size, 2, grid_size)`
+        encoder_input = torch.stack(
+            tensors=(
+                self.normalize_log_P(log_P),
+                self.normalize_T(T),
+            ),
+            dim=1,
+        )
+
+        # Compute forward pass through encoder to get latent variable z
+        x: torch.Tensor = self.convnet(encoder_input)
+        z: torch.Tensor = self.mlp(x)
 
         return z
 
