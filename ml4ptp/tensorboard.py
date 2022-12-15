a2150bbff212c424c3bb9394357d4e48351db23d
Timothy Gebhard <mail@timothygebhard.de>
2022-12-15 23:25:42 +0100

    Add custom TensorBoard logger

---
+++ ml4ptp/tensorboard.py
@@ -0,0 +1,39 @@
+"""
+Customized TensorBoard callback for PyTorch.
+"""
+
+# -----------------------------------------------------------------------------
+# IMPORTS
+# -----------------------------------------------------------------------------
+
+from typing import Mapping, Optional
+
+from pytorch_lightning import loggers
+from pytorch_lightning.utilities import rank_zero_only
+
+
+# -----------------------------------------------------------------------------
+# DEFINITIONS
+# -----------------------------------------------------------------------------
+
+class CustomTensorBoardLogger(loggers.TensorBoardLogger):
+    """
+    Customized TensorBoard callback for PyTorch.
+
+    This is a thin wrapper around the original TensorBoardLogger
+    callback, except it does not log the `epoch` to TensorBoard.
+    """
+
+    @rank_zero_only
+    def log_metrics(
+        self,
+        metrics: Mapping[str, float],
+        step: Optional[int] = None,
+    ) -> None:
+
+        # Drop the "epoch" from the metrics
+        metrics = dict(metrics)
+        metrics.pop('epoch', None)
+
+        # Call the original method
+        super().log_metrics(metrics, step)
