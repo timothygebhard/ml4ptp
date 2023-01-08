cdc8f1418918015e9d56f7a9369f60fd51ef4a64
Timothy Gebhard <mail@timothygebhard.de>
2023-01-08 20:12:26 +0100

    Refactor `find_optimal_z_with_ultranest()`
    - Running nested sampling is now time-limited
    - The prior can be chosen (either "uniform" or truncated "gaussian")
    - Use special dataclass for results instead of dictionary

--- ml4ptp/evaluation.py
+++ ml4ptp/evaluation.py
@@ -6,20 +6,49 @@ Functions for evaluation trained models.
 # IMPORTS
 # -----------------------------------------------------------------------------
 
-from typing import Dict, Union
+from dataclasses import dataclass
+from sys import stderr
 
 import logging
+import time
 import traceback
+
+from scipy.special import erf, erfinv
 
 import numpy as np
 import ultranest
 
 from ml4ptp.onnx import ONNXEncoder, ONNXDecoder
+from ml4ptp.timeout import timelimit, TimeoutException
 
 
 # -----------------------------------------------------------------------------
 # DEFINITIONS
 # -----------------------------------------------------------------------------
+
+@dataclass
+class EvaluationResult:
+    """
+    Simple dataclass for storing the results of an evaluation.
+    """
+
+    idx: int
+    log_P: np.ndarray
+    T_true: np.ndarray
+    z_initial: np.ndarray = np.empty(0)
+    z_refined: np.ndarray = np.empty(0)
+    T_pred_initial: np.ndarray = np.empty(0)
+    T_pred_refined: np.ndarray = np.empty(0)
+    mre_initial: float = np.nan
+    mre_refined: float = np.nan
+    mse_initial: float = np.nan
+    mse_refined: float = np.nan
+    niter: int = -1
+    ncall: int = -1
+    success: int = 0
+    runtime: float = np.nan
+    timeout : int = 0
+
 
 def find_optimal_z_with_ultranest(
     log_P: np.ndarray,
@@ -30,11 +59,39 @@ def find_optimal_z_with_ultranest(
     random_seed: int,
     n_live_points: int = 400,
     n_max_calls: int = 500_000,
-) -> Dict[str, Union[int, float, np.ndarray]]:
+    timeout: int = 600,
+    prior: str = 'uniform',
+    limit: float = 4.0,
+) -> EvaluationResult:
     """
     Use nested sampling to find the optimal latent variable `z` for a
     given PT profile.
+
+    Args:
+        log_P: The logarithm the pressure value of the profile.
+        T_true: The temperature value of the profile.
+        idx: The index of the profile.
+        encoder_bytes: The ONNX encoder model as a byte string.
+        decoder_bytes: The ONNX decoder model as a byte string.
+        random_seed: The random seed to use.
+        n_live_points: The number of live points to use.
+        n_max_calls: The maximum number of calls to the likelihood
+            function.
+        timeout: The maximum number of seconds for the nested sampling.
+        prior: The prior to use. Can be either 'uniform' or 'gaussian',
+            where the latter is a truncated Gaussian prior with mean 0
+            and standard deviation 1.
+        limit: The limit of the prior. For a uniform prior, this is the
+            range of the prior: `[-limit, limit]`. For the (truncated)
+            normal prior, this is the maximum norm of the proposed `z`.
+
+    Returns:
+        An `EvaluationResult` instance containing the results of the
+        evaluation using nested sampling.
     """
+
+    # Start the stopwatch
+    start_time = time.time()
 
     # Fix the shapes of the inputs
     log_P = log_P.reshape(1, -1)
@@ -44,8 +101,8 @@ def find_optimal_z_with_ultranest(
     encoder = ONNXEncoder(encoder_bytes)
     decoder = ONNXDecoder(decoder_bytes)
 
-    # Prepare dict with results
-    results: Dict[str, Union[int, float, np.ndarray]] = dict(
+    # Prepare result object
+    result = EvaluationResult(
         idx=idx,
         log_P=log_P,
         T_true=T_true,
@@ -55,36 +112,42 @@ def find_optimal_z_with_ultranest(
     # Get initial guess for z from encoder and compute error
     # -------------------------------------------------------------------------
 
-    # Get initial guess for z from encoder
+    # Get initial guess for z from encoder, and compute predicted T
     z_initial = encoder(log_P=log_P, T=T_true)
-
-    print('\n\n')
-    print('z_initial', z_initial, z_initial.shape)
-
     T_pred_initial = decoder(log_P=log_P, z=z_initial)
 
     # Compute mean squared error (MSE) and mean relative error (MRE)
     mse_initial = float(np.mean((T_pred_initial - T_true) ** 2))
     mre_initial = float(np.mean(np.abs(T_pred_initial - T_true) / T_true))
 
-    # Save results
-    results['z_initial'] = z_initial
-    results['T_pred_initial'] = T_pred_initial
-    results['mse_initial'] = mse_initial
-    results['mre_initial'] = mre_initial
+    # Store results
+    result.z_initial = z_initial
+    result.T_pred_initial = T_pred_initial
+    result.mse_initial = mse_initial
+    result.mre_initial = mre_initial
 
     # -------------------------------------------------------------------------
     # Define prior and likelihood
     # -------------------------------------------------------------------------
 
-    def prior(cube: np.ndarray) -> np.ndarray:
+    def prior_transform(cube: np.ndarray) -> np.ndarray:
         """
-        Prior for z. (Currently uniform in [-5, 5].)
+        Transformation of the unit hypercube to the prior.
+        Supports either a uniform prior or a truncated Gaussian prior.
         """
 
         z = cube.copy()
         for i in range(z.shape[1]):
-            z[:, i] = 10 * (z[:, i] - 0.5)
+
+            if prior == 'uniform':
+                z[:, i] = 2 * limit * (z[:, i] - 0.5)
+
+            elif prior == 'gaussian':
+                factor = erf(np.sqrt(2) * limit / 2)
+                z[:, i] = np.sqrt(2) * erfinv((2 * z[:, i] - 1) * factor)
+
+            else:
+                raise ValueError(f'Invalid prior "{prior}"!')
 
         return z
 
@@ -131,53 +194,77 @@ def find_optimal_z_with_ultranest(
             sampler = ultranest.ReactiveNestedSampler(
                 param_names=[f'z{i}' for i in range(z_initial.shape[1])],
                 loglike=likelihood,
-                transform=prior,
+                transform=prior_transform,
                 vectorized=True,
             )
 
-            # `frac_remain=0.05` seems to help with cases where the likelihood
-            # has a plateau, which results in many live points being removed
-            # without replacement, which then leads to an error when `nlive`
-            # becomes zero.
-            # noinspection PyTypeChecker
-            result = sampler.run(
-                min_num_live_points=n_live_points,
-                show_status=False,
-                viz_callback=False,
-                max_ncalls=n_max_calls,
-                frac_remain=0.05,
-            )
+            # Run sampler
+            # We limit the runtime to make sure that the evaluation will
+            # terminate in a reasonable and predictable amount of time.
+            with timelimit(timeout):
 
-            z_refined = np.asarray(result['maximum_likelihood']['point'])
-            results['z_refined'] = z_refined.squeeze()
-            results['ncall'] = int(result['ncall'])
-            results['niter'] = int(result['niter'])
-            results['success'] = int(
-                result['insertion_order_MWW_test']['converged']
-            )
+                # `frac_remain=0.05` seems to help with cases where the
+                # likelihood has a plateau, which results in many live points
+                # being removed without replacement, which then leads to an
+                # error when `nlive` becomes zero.
+                # noinspection PyTypeChecker
+                posterior = sampler.run(
+                    min_num_live_points=n_live_points,
+                    show_status=False,
+                    viz_callback=False,
+                    max_ncalls=n_max_calls,
+                    frac_remain=0.05,
+                )
 
-            # If we get here, we have successfully run the sampler, so we can
+            # If we get here, the evaluation was successful and did not time
+            # out, so we can continue with the results we have obtained, and
             # break out of the loop
+            z_refined = np.asarray(posterior['maximum_likelihood']['point'])
+            result.z_refined = z_refined.squeeze()
+            result.ncall = int(posterior['ncall'])
+            result.niter = int(posterior['niter'])
+            result.success = int(
+                posterior['insertion_order_MWW_test']['converged']
+            )
             break
 
         except Exception as e:  # pragma: no cover
 
-            print(f'\nError in ultranest (profile {idx}): {str(e)}\n')
-            print(traceback.format_exc())
-            n_tries += 1
+            # Check what kind of error has brought us here:
+
+            # If we have reached a time limit, we abort the evaluation and
+            # return the initial guess, because trying again will probably
+            # not help ...
+            if isinstance(e, TimeoutException):
+
+                print(f'Evaluation timed out! (profile {idx})', file=stderr)
+                z_refined = z_initial
+                result.z_refined = z_refined.squeeze()
+                result.timeout = 1
+                break
+
+            # Otherwise, we can log the error and try again
+            else:
+
+                print(
+                    f'\nError in ultranest (profile {idx}): {str(e)}\n',
+                    file=stderr
+                )
+                print(traceback.format_exc(), file=stderr)
+                n_tries += 1
 
     # The `else` clause is executed if we do not `break` out of the while loop,
-    # that is, if we did NOT find a solution with nested sampling.
+    # that is, if we did NOT find a solution with nested sampling, and did NOT
+    # time out. In this case, we return the initial guess.
     else:  # pragma: no cover
 
-        print('\n\nFailed to run ultranest!')
-        print(f'Using initial guess for profile {idx}.\n\n')
+        print(
+            f'Failed to run ultranest! Using initial guess for profile {idx}',
+            file=stderr
+        )
 
         z_refined = z_initial
-        results['z_refined'] = z_refined.squeeze()
-        results['ncall'] = -1
-        results['niter'] = -1
-        results['success'] = 0
+        result.z_refined = z_refined.squeeze()
 
     # -------------------------------------------------------------------------
     # Decode refined z and compute error
@@ -190,9 +277,10 @@ def find_optimal_z_with_ultranest(
     mse_refined = float(np.mean((T_pred_refined - T_true) ** 2))
     mre_refined = float(np.mean(np.abs(T_pred_refined - T_true) / T_true))
 
-    # Save results
-    results['T_pred_refined'] = T_pred_refined
-    results['mse_refined'] = mse_refined
-    results['mre_refined'] = mre_refined
+    # Store results
+    result.T_pred_refined = T_pred_refined
+    result.mse_refined = mse_refined
+    result.mre_refined = mre_refined
+    result.runtime = float(time.time() - start_time)
 
-    return results
+    return result
