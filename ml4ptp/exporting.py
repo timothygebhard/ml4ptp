b5db40eb1101acb39200b8d2d49fa83d049c754e
Timothy Gebhard <mail@timothygebhard.de>
2023-01-30 01:23:58 +0100

    Add optional clipping to `PTProfile`

--- ml4ptp/exporting.py
+++ ml4ptp/exporting.py
@@ -7,7 +7,7 @@ Utilities for exporting models.
 # -----------------------------------------------------------------------------
 
 from pathlib import Path
-from typing import Dict, Tuple, Union
+from typing import Dict, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -23,9 +23,41 @@ class PTProfile:
     """
     Wrapper class for loading a decoder model from a (ONNX) file that
     provides an intuitive interface for running the model.
+
+    Args:
+        path_or_bytes: The path to the ONNX file (or the file contents
+            loaded using ``onnx.load(file_path).SerializeToString()``)
+            that contains the trained decoder model.
+        log_P_min: The minimum value of the log of the pressure. If this
+            value is set, `T(log_p)` will be set to `T(log_P_min)` for
+            all `log_p` < `log_P_min`. If this value is set to None, no
+            such clipping will be performed.
+        log_P_max: The maximum value of the log of the pressure. If this
+            value is set, `T(log_p)` will be set to `T(log_P_max)` for
+            all `log_p` > `log_P_max`. If this value is set to None, no
+            such clipping will be performed.
+        T_min: The minimum value of the predicted temperature. If this
+            value is set, all `T` < `T_min` will be set to `T_min`.
+            Default: 0 (negative temperature could break the simulator).
+        T_max: The maximum value of the predicted temperature. If this
+            value is set, all `T` > `T_max` will be set to `T_max`.
+            Default: None.
     """
 
-    def __init__(self, path_or_bytes: Union[str, bytes]) -> None:
+    def __init__(
+        self,
+        path_or_bytes: Union[str, bytes],
+        log_P_min: Optional[float] = None,
+        log_P_max: Optional[float] = None,
+        T_min: Optional[float] = 0,
+        T_max: Optional[float] = None,
+    ) -> None:
+
+        # Store constructor arguments
+        self.log_P_min = log_P_min
+        self.log_P_max = log_P_max
+        self.T_min = T_min
+        self.T_max = T_max
 
         # Load the (decoder) model from the given file path
         self.model = ONNXDecoder(path_or_bytes)
@@ -43,6 +75,12 @@ class PTProfile:
         z = np.atleast_2d(z)
         log_P = np.atleast_2d(log_P)
 
+        # Clip the log_P values if necessary
+        if self.log_P_min is not None:
+            log_P = np.clip(log_P, self.log_P_min, None)
+        if self.log_P_max is not None:
+            log_P = np.clip(log_P, None, self.log_P_max)
+
         # Run some sanity checks on the shapes
         if not z.shape[1] == self.latent_size:
             raise ValueError(f'z must be {self.latent_size}-dimensional!')
@@ -51,6 +89,12 @@ class PTProfile:
 
         # Send through the model
         T = self.model(z=z, log_P=log_P)
+
+        # Clip the temperature values if necessary
+        if self.T_min is not None:
+            T = np.clip(T, self.T_min, None)
+        if self.T_max is not None:
+            T = np.clip(T, None, self.T_max)
 
         return np.asarray(np.atleast_1d(T.squeeze()))
 
